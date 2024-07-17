db27285fe341ac6b58d5452578fdf80bb1544379
asif <rimbaborne@gmail.com>
2024-07-17 22:26:09 +0800

    menu modul

---
+++ resources/views/components/kelas/menu-modul.blade.php
@@ -0,0 +1,96 @@
+<li class="p-4 bg-white rounded-lg">
+    <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-playground" data-collapse-toggle="dropdown-playground">
+        <svg sidebar-toggle-item="" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <span class="flex-1 ml-3 text-left whitespace-nowrap font-bold" sidebar-toggle-item="">
+            Materi Utama
+        </span>
+        {{-- <span class="text-right font-bold">
+            1 / 3
+        </span> --}}
+    </button>
+    <ul id="dropdown-playground" class="space-y-2 py-2">
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/1" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/1') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                Video Pembelajaran
+            </Link>
+        </li>
+
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/2" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/2') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                Rangkuman
+            </Link>
+        </li>
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/3" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/3') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                Slide Presentasi
+            </Link>
+        </li>
+    </ul>
+</li>
+<li class="p-4 bg-white rounded-lg">
+    <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-playground" data-collapse-toggle="dropdown-playground">
+        <svg sidebar-toggle-item="" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <span class="flex-1 ml-3 text-left whitespace-nowrap font-bold" sidebar-toggle-item="">
+            Bonus
+        </span>
+        {{-- <span class="text-right font-bold">
+            1 / 3
+        </span> --}}
+    </button>
+    <ul id="dropdown-playground" class="space-y-2 py-2">
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/1" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/1') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                .pdf
+            </Link>
+        </li>
+
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/2" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/2') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                .pdf
+            </Link>
+        </li>
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/3" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/3') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                .pdf
+            </Link>
+        </li>
+    </ul>
+</li>
+<li class="p-4 bg-white rounded-lg">
+    <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-playground" data-collapse-toggle="dropdown-playground">
+        <svg sidebar-toggle-item="" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        <span class="flex-1 ml-3 text-left whitespace-nowrap font-bold" sidebar-toggle-item="">
+            Fasilitas Tambahan
+        </span>
+        {{-- <span class="text-right font-bold">
+            1 / 3
+        </span> --}}
+    </button>
+    <ul id="dropdown-playground" class="space-y-2 py-2">
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/1" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/1') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                Konsultasi (Link chat WA)
+            </Link>
+        </li>
+
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/2" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/2') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                Customer Care (Link chat WA)
+            </Link>
+        </li>
+        <li>
+            <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/3" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/3') ? 'bg-slate-200' : '' }}">
+                <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
+                Tips Bisnis Mingguan (Link ke WA)
+            </Link>
+        </li>
+    </ul>
+</li>
--- resources/views/pages/kelas-profit.blade.php
+++ resources/views/pages/kelas-profit.blade.php
@@ -63,38 +63,7 @@
             <div class="h-full overflow-y-auto flex-1 divide-y md:bg-transparent bg-white rounded-lg divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                 <ul class="pb-4 mb-6 space-y-2">
                     {{-- @for ($i = 1; $i < 7; $i++) --}}
-                    <li class="p-4 bg-white rounded-lg">
-                        <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-playground" data-collapse-toggle="dropdown-playground">
-                            <svg sidebar-toggle-item="" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            <span class="flex-1 ml-3 text-left whitespace-nowrap font-bold" sidebar-toggle-item="">
-                                Modul Pembelajaran 1
-                            </span>
-                            <span class="text-right font-bold">
-                                1 / 3
-                            </span>
-                        </button>
-                        <ul id="dropdown-playground" class="space-y-2 py-2">
-                            <li>
-                                <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/1" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/1') ? 'bg-slate-200' : '' }}">
-                                    <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
-                                    Materi 1
-                                </Link>
-                            </li>
-
-                            <li>
-                                <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/2" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/2') ? 'bg-slate-200' : '' }}">
-                                    <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
-                                    Materi 2
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="{{ url('/') }}/kelas/kelas-profit-10-juta/3" class="text-base ml-4 text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-400 transition duration-75 dark:text-gray-200 dark:hover:bg-gray-700 {{ request()->is('kelas/kelas-profit-10-juta/3') ? 'bg-slate-200' : '' }}">
-                                    <x-carbon-triangle-right-solid class="w-2 h-2 mx-4" />
-                                    Materi 3
-                                </Link>
-                            </li>
-                        </ul>
-                    </li>
+                    @include('components.kelas.menu-modul')
                     {{-- @endfor --}}
 
                     <li class="pb-6"></li>
