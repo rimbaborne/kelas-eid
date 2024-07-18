7850b6850bfd647055732116c1bebafb23ac827b
asif <rimbaborne@gmail.com>
2024-07-18 12:38:58 +0800

    ;layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,23 +11,23 @@
 
     <x-auth-card>
         <h1 class="text-2xl font-bold text-center py-4">Pemesanan</h1>
-        <div class="flex flex-col border justify-center bg-white rounded-xl items-center">
+        <div class="flex flex-col border justify-center bg-white rounded-xl items-center mb-4">
             <div class="rounded-lg bg-white p-2 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-4">
-                    <div class="space-y-2 flex items-center justify-between gap-6 md:space-y-0">
-                        <a href="#" class="shrink-0">
-                            <img class="w-32 p-2 sm:p-3" src="https://admin.entrepreneurid.org/img/produk/KPS-2024-1.png">
-                        </a>
+                <div class="space-y-2 flex items-center justify-between gap-6 md:space-y-0">
+                    <a href="#" class="shrink-0">
+                        <img class="w-28 p-2 sm:p-3" src="https://admin.entrepreneurid.org/img/produk/KPS-2024-1.png">
+                    </a>
 
-                        <div class="w-full min-w-0 flex-1 space-y-2 md:max-w-md">
-                            <h1 class="text-xl font-semibold text-gray-900 hover:underline dark:text-white">
-                                Kelas Profit 10 Juta
-                            </h1>
-                            <h1 class="text-lg font-semibold text-gray-900 hover:underline dark:text-white">
-                                Rp. 57.000
-                            </h1>
-                        </div>
+                    <div class="w-full min-w-0 flex-1 space-y-2 md:max-w-md">
+                        <h1 class="text-xl font-semibold text-gray-900 hover:underline dark:text-white">
+                            Kelas Profit 10 Juta
+                        </h1>
+                        <h1 class="text-lg font-semibold text-gray-900 hover:underline dark:text-white">
+                            Rp. 57.000
+                        </h1>
                     </div>
                 </div>
+            </div>
         </div>
         <x-splade-form action="{{ route('register') }}" class="space-y-4"
             confirm-text="Apakah data yang anda masukkan sudah benar ?" confirm="Konfirmasi" confirm-button="Benar"
