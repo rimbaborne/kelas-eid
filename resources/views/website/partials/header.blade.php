c1ac854e713ca3b00f2dab6b526fbb38845d22e0
asif <rimbaborne@gmail.com>
2024-07-20 06:15:10 +0800

    fix minimalis launch

--- resources/views/website/partials/header.blade.php
+++ resources/views/website/partials/header.blade.php
@@ -6,15 +6,7 @@
                 {{-- <h3 class="font-extrabold leading-none tracking-tight">Kelas entrepreneurID</h3> --}}
             </Link>
             <div class="flex items-center lg:order-2">
-                @if(Auth::check())
-                    <Link href="{{ url('/dashboard') }}" class="text-gray-800 border border-gray-400 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
-                        Dashboard
-                    </Link>
-                @else
-                    <Link href="{{ url('/login') }}" class="text-gray-800 border border-gray-400 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
-                        Login
-                    </Link>
-                @endif
+
                 {{-- <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">PPDB</a> --}}
                 <button id="togglewebmenuMobile" aria-expanded="true" aria-controls="webmenu" class="p-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 ">
                     <svg id="togglewebmenuMobileHamburger" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
