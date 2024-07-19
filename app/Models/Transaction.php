ec6aa7076b99fe10fee39b5ecd3eb619049530c1
asif <rimbaborne@gmail.com>
2024-07-20 07:17:48 +0800

    payment code

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -26,6 +26,7 @@ class Transaction extends Model
         'payment_name',
         'status_desc',
         'status_pembayaran',
+        'status_pembayaran_code',
         'qris_string',
         'qris_nmid',
     ];
