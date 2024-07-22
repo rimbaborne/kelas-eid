a9ff00e1a9bafc7daae9a657643f73c078b0d786
asif <rimbaborne@gmail.com>
2024-07-22 20:08:08 +0800

    middleware config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,18 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->group('splade', [\ProtoneMedia\Splade\Http\SpladeMiddleware::class]);
-        //
-    })
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->group('splade', [\ProtoneMedia\Splade\Http\SpladeMiddleware::class]);
+    $middleware->use([\App\Http\Middleware\VerifyCsrfToken::class]);
+    //
+})
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->renderable(\ProtoneMedia\Splade\SpladeCore::exceptionHandler($exceptions->handler));
         //
