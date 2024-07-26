a78995b06a91a43fde3c3f5955722fa0fb4d0b36
asif <rimbaborne@gmail.com>
2024-07-26 18:22:46 +0800

    redaksi

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -56,43 +56,43 @@ class KelasController extends BaseController
                 [ 'nama' => 'Waktu Pilihan Allah', 'link' => '9' ],
             ];
             if($link == 1) {
-                $judul = 'Vitamin Finansial';
-                $file  = 'entrepreneurID - Ebook Vitamin Finansial';
+                $judul = 'Ebook Vitamin Finansial';
+                $file  = 'entrepreneurID - Vitamin Finansial';
                 $pdf_  = 'https://drive.google.com/file/d/1H_PPQ06b8UU-tZRBRwgsOqp7eY9hN59X/preview';
             } elseif($link == 2) {
-                $judul = 'Mulai dari Awal Lagi';
-                $file  = 'entrepreneurID - Ebook Mulai dari Awal Lagi';
+                $judul = 'Ebook Mulai dari Awal Lagi';
+                $file  = 'entrepreneurID - Mulai dari Awal Lagi';
                 $pdf_  = 'https://drive.google.com/file/d/1pnqGmp7FeHx6cW_ZFAPp-m4ANvPcfA1E/preview';
             } elseif($link == 3) {
-                $judul = '7 Ide Bisnis Puluan Juta';
-                $file  = 'entrepreneurID - Ebook 7 Ide Bisnis Puluan Juta';
+                $judul = 'Ebook 7 Ide Bisnis Puluan Juta';
+                $file  = 'entrepreneurID - 7 Ide Bisnis Puluan Juta';
                 $pdf_  = 'https://drive.google.com/file/d/15lAN1JK-oJbu0xQ5Ri4JKmKCC5B6tZ6r/preview';
             } elseif($link == 4) {
-                $judul = 'Jangan Dulu Bisnis Sebelum Punya Ini';
-                $file  = 'entrepreneurID - Ebook Jangan Dulu Bisnis Sebelum Punya Ini';
+                $judul = 'Ebook Jangan Dulu Bisnis Sebelum Punya Ini';
+                $file  = 'entrepreneurID - Jangan Dulu Bisnis Sebelum Punya Ini';
                 $pdf_  = 'https://drive.google.com/file/d/1OtdCgkxvfNZnp5xGwYVtG03PxVqIFxXj/preview';
             } elseif($link == 5) {
-                $judul = 'Agar Bisnismu Terus Tumbuh';
-                $file  = 'entrepreneurID - Ebook Agar Bisnismu Terus Tumbuh';
+                $judul = 'Ebook Agar Bisnismu Terus Tumbuh';
+                $file  = 'entrepreneurID - Agar Bisnismu Terus Tumbuh';
                 $pdf_  = 'https://drive.google.com/file/d/1y3NvPW23It0oUViNY1i_djjlZJIiuBUy/preview';
             } elseif($link == 6) {
-                $judul = 'Tembus Target Jualan Online';
-                $file  = 'entrepreneurID - Ebook Tembus Target Jualan Online';
+                $judul = 'Ebook Tembus Target Jualan Online';
+                $file  = 'entrepreneurID - Tembus Target Jualan Online';
                 $pdf_  = 'https://drive.google.com/file/d/1Ug7C0NnfxxoCQ--a16e9rKc5L_9l2_Wo/preview';
             } elseif($link == 7) {
-                $judul = 'Rahasia Jualan Anti Gagal';
-                $file  = 'entrepreneurID - Ebook Rahasia Jualan Anti Gagal';
+                $judul = 'Ebook Rahasia Jualan Anti Gagal';
+                $file  = 'entrepreneurID - Rahasia Jualan Anti Gagal';
                 $pdf_  = 'https://drive.google.com/file/d/1ZxQ75M_1TpamocVzKVP_yVL4v16kjy5w/preview';
             } elseif($link == 8) {
-                $judul = 'Cara Konsisten Tanpa Tekanan';
-                $file  = 'entrepreneurID - Ebook Cara Konsisten Tanpa Tekanan';
+                $judul = 'Ebook Cara Konsisten Tanpa Tekanan';
+                $file  = 'entrepreneurID - Cara Konsisten Tanpa Tekanan';
                 $pdf_  = 'https://drive.google.com/file/d/1SpJQDQFWIlif_gEy3yPKm-2CBA7y9Tn7/preview';
             } elseif($link == 9) {
-                $judul = 'Waktu Pilihan Allah';
-                $file  = 'entrepreneurID - Ebook Waktu Pilihan Allah';
+                $judul = 'Ebook Waktu Pilihan Allah';
+                $file  = 'entrepreneurID - Waktu Pilihan Allah';
                 $pdf_  = 'https://drive.google.com/file/d/1TJr6EmN5eU9yskmbr-0gRsm-VQcido57/preview';
             } else {
-                $judul = 'Kelas Tidak Ada';
+                $judul = 'Ebook Kelas Tidak Ada';
                 $pdf_  = 'none';
             }
             return view('pages.kelas-profit-pdf', compact('pdf_', 'judul', 'file'));
