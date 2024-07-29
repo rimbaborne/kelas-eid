1996adaec39a5cf8608198f035ae2e24dfbed065
asif <rimbaborne@gmail.com>
2024-07-29 23:00:28 +0800

    fix

--- resources/views/components/kelas/menu-modul.blade.php
+++ resources/views/components/kelas/menu-modul.blade.php
@@ -1,6 +1,6 @@
 <li class="p-4 bg-white rounded-lg">
-    <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-playground-1" data-collapse-toggle="dropdown-playground-1">
-        <span class="flex-1 ml-3 text-left whitespace-nowrap font-bold" sidebar-toggle-item="">
+    <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" >
+        <span class="flex-1 ml-3 text-left whitespace-nowrap font-bold">
             KELAS PROFIT 10 JUTA
         </span>
         {{-- <span class="text-right font-bold">
